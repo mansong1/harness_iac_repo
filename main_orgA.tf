43e646cf5e15baac53351799fca99f1899b51d22
mansong1 <martin.ansong@harness.io>
2023-12-19 13:00:42 +0000

    orgA projectA kDd6lhCRTyu-6Vq8nCce4Q

---
+++ main_orgA.tf
@@ -0,0 +1,10 @@
+module "organization_orga" {
+  name   = "orgA"
+  source = "harness-community/structure/harness//modules/organizations"
+}
+module "project_projectA" {
+  name            = "projectA"
+  organization_id = "module.organization_orga_details.id"
+  color           = "#ffcc5c"
+  source          = "harness-community/structure/harness//modules/projects"
+}
